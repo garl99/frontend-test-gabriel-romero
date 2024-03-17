876cfeeffb07bede18dcd93fde20d098a4273d35
Gabriel Romero <garomerol.17@est.ucab.edu.ve>
2024-03-17 17:30:53 -0400

    feat: integrations

---
+++ src/app/sections/02-integrations/index.tsx
@@ -0,0 +1,23 @@
+import { Chip } from "@/components/chip";
+import { Text, TextVariant } from "@/components/text";
+import React from "react";
+import IntegrationsList from "./integrations-list";
+
+export default function Integrations() {
+  return (
+    <section className="flex flex-col justify-center items-center mt-5 px-8 md:px-32">
+      <Chip text="Integrations" />
+      <Text
+        variant={TextVariant.PrimaryMd}
+        text="Get more value from your tools"
+        className="mt-4 text-center"
+      />
+      <Text
+        variant={TextVariant.GrayLg}
+        text="Connect your tools, connect your teams. With over 100 apps already available in our directory, your team’s favourite tools are just a click away."
+        className="mt-5 text-center"
+      />
+      <IntegrationsList />
+    </section>
+  );
+}
