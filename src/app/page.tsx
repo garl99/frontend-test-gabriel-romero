d90491ba7caaeca88d0da1bef0f245aec86fccce
Gabriel Romero <garomerol.17@est.ucab.edu.ve>
2024-03-17 19:22:54 -0400

    feat: cta section part I

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./sections/01-header";
 import Integrations from "./sections/02-integrations";
+import CTA from "./sections/03-cta";
 
 export default function Home() {
   return (
     <main className="p-0 md:px-16 md:py-3">
       <Header />
       <Integrations />
+      <CTA />
     </main>
   );
 }
