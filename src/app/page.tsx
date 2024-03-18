8d2dd924c73d9ddd602bd13d1cb9b8d8cd42a168
Gabriel Romero <garomerol.17@est.ucab.edu.ve>
2024-03-17 22:26:12 -0400

    feat: faq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FirstCTA from "./sections/03-cta";
 import SecondCTA from "./sections/05-cta";
 import Features from "./sections/04-features";
 import Testimonials from "./sections/07-testimonial";
+import FAQ from "./sections/06-faq";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <FirstCTA />
       <Features />
       <SecondCTA />
+      <FAQ />
       <Testimonials />
     </main>
   );
